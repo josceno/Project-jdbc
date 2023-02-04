57a03c7a94242f0cca25618fe229ba17684df8ce
josceno <jdamasceno414@gmail.com>
2023-02-04 11:33:24 -0300

    using a databese created beforehand

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -43,5 +41,27 @@ public class DB {
         }
 
     }
+    public static  void closeStatment(Statement st) {
+        if(st !=null){
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw  new DbException("statement already closed");
+            }
+        }
 
-}
+    }
+    public static  void closeResultSet(ResultSet rs) {
+        if(rs !=null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw  new DbException("Result already closed");
+            }
+        }
+
+    }
+
+
+
+    }
