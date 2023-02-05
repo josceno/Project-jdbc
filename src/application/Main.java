59f00bf3bc03fb6e5c8096b6c69d3c129bd0f21e
josceno <jdamasceno414@gmail.com>
2023-02-05 11:22:26 -0300

    inserting with prepered statment

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,31 +2,68 @@ package application;
 
 import db.DB;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.swing.text.DefaultEditorKit;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.SimpleFormatter;
 
 public class Main {
-    public static void main(String[] args) {
-        Connection connection = null;
-        Statement st = null;
-        ResultSet rs = null;
-        try{
-            connection= DB.getConnection();
-            st = connection.createStatement();
+    public static void main(String[] args)  {
+       SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-            rs =  st.executeQuery("select * from department");
-            while (rs.next()){
-                System.out.println(rs.getInt("Id")+ ","+rs.getString("Name"));
+       Connection connection= null;
+       PreparedStatement pst = null;
+       ResultSet resultSet = null;
 
-            }
-        }catch (SQLException e){
-                e.printStackTrace();
-        }finally {
-            DB.closeResultSet(rs);
-            DB.closeStatment(st);
-            DB.closeConnection();
-        }
+       try {
+           connection = DB.getConnection();
+
+           pst  = connection.prepareStatement(
+                   "INSERT INTO seller "+
+                           "(Name,Email,BirthDate,BaseSalary,DepartmentId)"+
+                           "VALUES "+
+                           "(?,?,?,?,?)",Statement.RETURN_GENERATED_KEYS);
+           pst.setString(1,"Carl purple");
+           pst.setString(2,"carl@gmail.com");
+           pst.setDate(3, new java.sql.Date(sdf.parse("22/04/1984").getTime()));
+           pst.setDouble(4,3000.0);
+           pst.setInt(5,4);
+
+           int rowsAffected = pst.executeUpdate();
+           if(rowsAffected >0){
+              ResultSet rs = pst.getGeneratedKeys();
+
+              while (rs.next()){
+                  int id = rs.getInt(1);
+                  System.out.println("Done Id =" + id);
+              }
+           }else {
+               System.out.println("no rowls affected.");
+           }
+
+
+           resultSet = pst.executeQuery("select * from seller");
+           while(resultSet.next()){
+               System.out.println(resultSet.getInt(1)+","
+                       +resultSet.getString(2)+","
+                       +resultSet.getString(3)+","
+                       +sdf.format(resultSet.getDate(4))
+               );
+
+
+           }
+
+           System.out.println("Done Rows affected: "+ rowsAffected);
+
+       }catch (SQLException e){
+           e.printStackTrace();
+       } catch (ParseException e) {
+           throw new RuntimeException(e);
+       }finally {
+           DB.closeStatment(pst);
+           DB.closeResultSet(resultSet);
+           DB.closeConnection();
+       }
     }
 }
