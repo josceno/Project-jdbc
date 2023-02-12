14c411b6f17761da17ff538adfba793c273deffd
josceno <jdamasceno414@gmail.com>
2023-02-12 11:23:06 -0300

    Dao factury + (SellerDao)

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,8 @@ package application;
 
 import db.DB;
 import db.DbException;
+import models.Dao.DaoFactory;
+import models.Dao.SellerDao;
 import models.entities.Department;
 import models.entities.Seller;
 
@@ -16,6 +18,8 @@ import java.text.SimpleDateFormat;
 public class Main {
     public static void main(String[] args)   {
        Seller seller = new Seller(1,"Bob","bob@gmail.com",new Date(1,1,1), 3000.0, new Department(1,"dep"));
+       SellerDao sellerDao = DaoFactory.createSellerDao();
+
        System.out.println(seller);
     }
 }
